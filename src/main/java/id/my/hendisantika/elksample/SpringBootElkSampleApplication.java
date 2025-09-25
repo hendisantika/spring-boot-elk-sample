01e5dbd0a2ecc4fa0a7097f5289553217c77c625
hendisantika <hendisantika@yahoo.co.id>
2025-09-25 16:52:17 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/elksample/SpringBootElkSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.elksample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElkSampleApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootElkSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/elksample/SpringBootElkSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.elksample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElkSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
