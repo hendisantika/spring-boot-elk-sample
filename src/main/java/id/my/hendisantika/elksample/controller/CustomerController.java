21b50aafccfe323ecaad6c629fe6d273038527ac
hendisantika <hendisantika@yahoo.co.id>
2025-09-25 16:59:12 +0700

    feat: Add getCustomerById in CustomerController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/elksample/controller/CustomerController.java
+++ src/main/java/id/my/hendisantika/elksample/controller/CustomerController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -36,5 +37,14 @@ public class CustomerController {
         log.info("END - getAllCustomers");
 
         return ResponseEntity.ok(customers);
+    }
+
+    @GetMapping
+    public ResponseEntity<Customer> getCustomerById(@RequestParam("customerId") String id) {
+        log.info("START - getCustomerById, id: {}", id);
+        var customer = customerService.getCustomerById(id);
+        log.info("END - getCustomerById");
+
+        return ResponseEntity.ok(customer);
     }
 }
