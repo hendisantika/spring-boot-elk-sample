6a579eda902b61934867a45d1d31647a1be1e624
hendisantika <hendisantika@yahoo.co.id>
2025-09-25 16:58:59 +0700

    feat: Add getAllCustomers in CustomerController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/elksample/controller/CustomerController.java
+++ src/main/java/id/my/hendisantika/elksample/controller/CustomerController.java
@@ -1,10 +1,15 @@
 package id.my.hendisantika.elksample.controller;
 
+import id.my.hendisantika.elksample.entity.Customer;
 import id.my.hendisantika.elksample.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +28,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CustomerController {
     private final CustomerService customerService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        log.info("START - getAllCustomers");
+        var customers = customerService.listAllCustomers();
+        log.info("END - getAllCustomers");
+
+        return ResponseEntity.ok(customers);
+    }
 }
